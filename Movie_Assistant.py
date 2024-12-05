5111b5db076d72ffcab8169a452b8975933b329a
PimpitchaL <6740168522@student.chula.ac.th>
2024-12-05 18:41:13 +0700

    Update Movie_Assistant.py

--- Movie_Assistant.py
+++ Movie_Assistant.py
@@ -8,22 +8,19 @@ user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 openai.api_key = user_api_key
 
-prompt = """Act as an AI writing tutor in English. You will receive a 
-            piece of writing and you should give suggestions on how to improve it.
-            List the suggestions in a JSON array, one suggestion per line.
-            Each suggestion should have 4 fields:
-            - "before" - the text before the suggestion
-            - "after" - the text after the suggestion
-            - "category" - the category of the suggestion one of "grammar", "style", "word choice", "other"
-            - "comment" - a comment about the suggestion
-            Don't say anything at first. Wait for the user to say something.
-        """    
+#client = openai.OpenAI(api_key=user_api_key)
+prompt = """ Act as an AI assistant in finding information about movies in English and Thai.
+You will receive a name of a movie and you should provide information about the legal streaming platform
+containing said movie available in Thailand region. 
+Also suggest three other movies made by the same filmmaker below with short description about the movies.
+Please respond with a JSON object containing the following fields: "movie_title", "streaming_platform", "director", "other_movie".
+"""
 
-st.title('Writing tutor')
-st.markdown('Input the writing that you want to improve. \n\
-            The AI will give you suggestions on how to improve it.')
+st.title("Movie Platform :clapper:")
+st.subheader("Let's watch a movie while also supporting the makers!!")
+st.markdown("The AI can assist you in finding your favorite movie on online platforms!")
 
-user_input = st.text_area("Enter some text to correct:", "Your text here")
+user_input = st.text_area("Enter movie name:", "Your input here")
 
 # Submit button after text input
 if st.button('Submit'):
