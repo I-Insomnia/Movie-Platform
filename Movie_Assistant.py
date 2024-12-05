e5b3316b25af5e240375d068846ea94069b88e55
PimpitchaL <6740168522@student.chula.ac.th>
2024-12-05 19:43:11 +0700

    Update Movie_Assistant.py

--- Movie_Assistant.py
+++ Movie_Assistant.py
@@ -34,20 +34,18 @@ if st.button('Submit'):
                 model="gpt-3.5-turbo",
                 messages=messages_so_far
             )
-            # Extract the AI's response
+            
             suggestion_dictionary = response.choices[0].message.content
-
-            # Parse JSON and convert to DataFrame
+            
             try:
                 sd = json.loads(suggestion_dictionary)
                 suggestion_df = pd.DataFrame.from_dict(sd)
-                st.markdown('**AI response:**')
                 st.write(f"**Movie Title:** {sd['movie_title']}")
                 st.write(f"**Streaming Platform:** {sd['streaming_platform']}")
                 st.write(f"**Director:** {sd['director']}")
                 st.write(f"**Suggestions movie from the same director:**")
                 st.table(sd['other_movie'])
-                #st.table(suggestion_df)
+    
             except json.JSONDecodeError:
                 st.error("The response is not in valid JSON format. Please try again.")
 
